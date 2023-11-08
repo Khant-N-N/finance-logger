8e4a157786a82cadda4c67e838345a7ee6c56e80
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2023-11-09 00:14:22 +0630

    add another way

--- src/classes/payment.ts
+++ src/classes/payment.ts
@@ -1,15 +1,12 @@
 import { hasFormatter } from "../interfaces/hasFormatter.js";
 
 export class payment implements hasFormatter {
-    readonly recipient : string; // can access anywhere but can't change
-    private details : string; // cannot access outside of this class
-    public amount : number; //can access any where and mutate
-    constructor(c:string, d:string, a: number){
-        this.recipient = c;
-        this.details = d;
-        this.amount = a;
-    }
-    format() {
-        return `${this.recipient} is owed ${this.amount} for ${this.details}.`
-    } 
+  constructor(
+    readonly recipient: string,
+    private details: string,
+    public amount: number
+  ) {}
+  format() {
+    return `${this.recipient} is owed ${this.amount} for ${this.details}.`;
+  }
 }
--- src/script.ts
+++ src/script.ts
@@ -2,26 +2,27 @@ import { invoice } from "./classes/invoice.js";
 import { payment } from "./classes/payment.js";
 import { hasFormatter } from "./interfaces/hasFormatter.js";
 
-const outputListContainer = document.querySelector<HTMLUListElement>(".item-list");
+const outputListContainer =
+  document.querySelector<HTMLUListElement>(".item-list")!;
 const typeSelector = document.querySelector<HTMLSelectElement>("#type");
 const amount = document.querySelector("#amount") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 
-document.querySelector(".new-item-form")?.addEventListener("submit", (e) => {
+document
+  .querySelector(".new-item-form")
+  ?.addEventListener("submit", (e: Event) => {
     e.preventDefault();
-    let doc: hasFormatter;
-    if (typeSelector?.value === "invoice") {
-        doc = new invoice(tofrom?.value, details?.value, amount?.valueAsNumber)
-    }else{
-        doc = new payment(tofrom.value, details.value, amount.valueAsNumber)
-    }
-    console.log(doc)
+    let doc: hasFormatter =
+      typeSelector?.value === "invoice"
+        ? new invoice(tofrom?.value, details?.value, amount?.valueAsNumber)
+        : new payment(tofrom.value, details.value, amount.valueAsNumber);
+
     const list = document.createElement("li");
     const header: any = document.createElement("h4");
     header.textContent = typeSelector?.value;
     const para = document.createElement<any>("p");
-    para.textContent= doc.format();
+    para.textContent = doc.format();
     list.append(header, para);
-    outputListContainer?.appendChild(list)
-})
+    outputListContainer?.appendChild(list);
+  });
